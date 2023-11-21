1c5960c71c128b542245077d7532fa3f1457dd58
Faxe <78741497+FaxeLeDev@users.noreply.github.com>
2023-11-21 10:45:55 +0000

    Début du travail

---
+++ index.php
@@ -0,0 +1,23 @@
+<html> 
+<head>
+    <title>Faxe</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">
+            <img src="assets/src/logo.svg" alt="Logo">
+            <h1>Faxe</h1>
+        </div>
+        <nav class="raccourcis">
+            <ul>
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Devis</a></li>
+                <li><a href="#">Portfolio</a></li>
+            </ul>
+        </nav>
+    <button class="contact">Contact</button>
+    </div>
+</body>
+</html>
